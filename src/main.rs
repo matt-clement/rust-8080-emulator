7a9fdb4b0643c3158d7b77cc91aac88bb4ddb173
Matt Clement <clement.matthewp@gmail.com>
2020-04-03 05:30:45 +0000

    Working SDL2 demo code

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,46 @@ mod state_8080;
 
 use state_8080::State8080;
 
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use std::time::Duration;
+
 fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+      .position_centered()
+      .build()
+      .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut i = 0;
+    'running: loop {
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), ..} => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+}
+
+fn dothething() {
     let file_name = std::env::args().nth(1).expect("Pass file name as first argument");
     let mut file = File::open(&file_name).expect(&format!("Unable to open file '{}'", file_name));
     let mut buffer: Vec<u8> = Vec::new();
