32582c5e733734e445c88c5aa48f2afdc4cf5251
Matt Clement <clement.matthewp@gmail.com>
2020-04-08 02:12:10 +0000

    Implement ANI instruction

--- src/emulator.rs
+++ src/emulator.rs
@@ -1191,7 +1191,15 @@ pub fn emulate_8080_op(state: &mut State8080) -> u32 {
             state.memory[state.sp as usize - 2] = state.l;
             state.sp -= 2;
         },
-        0xe6 => unimplemented_instruction(state),
+        0xe6 => {
+            let answer: u8 = state.a & state.memory[program_counter + 1];
+            state.cc.z = if answer == 0 { 1 } else { 0 };
+            state.cc.s = if (answer & 0x80) == 0x80 { 1 } else { 0 };
+            state.cc.cy = 0;
+            state.cc.p = parity(answer);
+            state.a = answer;
+            state.increment_program_counter(1);
+        },
         0xe7 => unimplemented_instruction(state),
         0xe8 => {
             if state.cc.p != 0 {
@@ -1845,5 +1853,14 @@ mod test {
         emulate_8080_op(&mut state);
         // TODO: assert_eq!(state.cc.cy, 0);
         assert_eq!(state.cc.z, 0);
+    }
+
+    #[test]
+    fn test_ani() {
+        let mut state = State8080::empty_state();
+        state.memory = vec![0xe6, 0x0f];
+        state.a = 0x3a;
+        emulate_8080_op(&mut state);
+        assert_eq!(state.a, 0x0a);
     }
 }
